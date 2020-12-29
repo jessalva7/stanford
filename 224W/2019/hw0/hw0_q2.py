e324443c0f36930858af633791e95cd63ab33e41
Jessal <jessal.va@outlook.com>
2020-12-29 12:31:21 +0530

    completed hw0 qn2

---
+++ 224W/2019/hw0/hw0_q2.py
@@ -0,0 +1,33 @@
+from hw0_q1 import extract_Graph
+import snap
+import matplotlib.pyplot as plt 
+import numpy as np
+import math
+
+def x_out_degree_node_classification(wikiGraph:snap.TNGraph):
+
+    out_degree_map = {}
+    min_x, max_x = 1, 0
+    for node in wikiGraph.Nodes():
+        if node.GetOutDeg() not in out_degree_map:
+            out_degree_map[ node.GetOutDeg() ] = 0
+        out_degree_map[ node.GetOutDeg() ] += 1
+        min_x = min(node.GetOutDeg(), min_x )
+        max_x = max(node.GetOutDeg(), max_x )
+    return out_degree_map, min_x, max_x
+
+wikiGraph = extract_Graph()
+
+map, min_x, max_x = x_out_degree_node_classification(wikiGraph)
+x = list(map.keys())
+x.sort()   
+y = [ map[i] for i in x ]
+
+
+slope, intercept = np.polyfit( np.log10(np.array(x[1:])), np.log10(np.array(y[1:])), 1 )
+linear_regressor_line = slope * np.log10(np.array(x[1:])) + intercept
+plt.figure(figsize=(4, 3), dpi=70)
+plt.loglog(x, y )
+plt.loglog(x[1:], linear_regressor_line, '-')
+plt.savefig("output.png", dpi=400)
+plt.show()
