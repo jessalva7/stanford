29a1c4f44903aabd76a21a106778461c93cc3d7f
Jessal <jessal.va@outlook.com>
2021-02-02 00:07:57 +0530

    trying kronecker product

---
+++ 224W/2019/kronecker_product.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def nth_kron_product(arr : np.ndarray, n : int) -> np.ndarray:
+    if n < 0 :
+        return arr 
+    intermediate_result = arr
+    for i in range( n ):
+        intermediate_result = np.kron(intermediate_result, arr)
+    return intermediate_result
+
+
+a = np.array([[1,1],[1,0]])
+print(nth_kron_product(a,20).shape)
